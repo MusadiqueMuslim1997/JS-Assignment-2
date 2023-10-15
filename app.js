b49ab701e6b3f6315b771c61baa34b79e6603e0b
Musadique Muslim <musadique1997@gmail.com>
2023-10-15 20:32:52 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var a
+var operator
+var c
+var result
+a = +prompt("Enter First Value")
+
+operator = prompt("Used any one operator (+ , - , * , / , % )")
+
+c = +prompt("Enter Second Value")
+
+
+if(operator == "+"){
+    alert(result = a+c)
+}
+else if(operator == "-"){
+alert(a-c)
+    
+}
+else if(operator == "*"){
+alert(a*c)
+    
+}
+else if(operator == "/"){
+alert(a/c)
+    
+}
+else if(operator == "%"){
+alert(a%c)
+    
+}
+else{
+    alert("Try again")
+}
+
